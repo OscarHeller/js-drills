cb56f0569a4c835e4d4c1bb899af534a43f69e89
Oscar Heller <oheller25@gmail.com>
2026-09-02 23:43:23 -0400

    Added missing space to match house style.

--- event-loop/03.js
+++ event-loop/03.js
@@ -4,7 +4,7 @@ setTimeout(() => console.log(`A @${t()}ms`), 40);
 setTimeout(() => console.log(`B @${t()}ms`), 20);
 Promise.resolve().then(() => {
     const s = Date.now();
-    while (Date.now() -s < 30) {}
+    while (Date.now() - s < 30) {}
     console.log(`C @${t()}ms`);
 });
 console.log(`D @${t()}ms`);
